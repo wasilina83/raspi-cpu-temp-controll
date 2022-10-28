9dcaf5f083d7273c31135d3adb5fb14830b6692e
wasilina83 <103425290+wasilina83@users.noreply.github.com>
2022-10-28 02:30:06 +0200

    Update get-cpu-temp.cpp

--- get-cpu-temp.cpp
+++ get-cpu-temp.cpp
@@ -1,1 +1,13 @@
 
+
+td::string fileName = "/sys/class/thermal/thermal_zone0/temp";
+std::ifstream piCpuTempFile;
+float piCpuTemp = 0.0;
+std::stringstream buffer;
+piCpuTempFile.open(fileName);
+buffer << piCpuTempFile.rdbuf();
+piCpuTempFile.close();
+piCpuTemp = std::stof(buffer.str()); // convert string to float
+piCpuTemp = piCpuTemp / 1000; // convert float value to degree
+piCpuTemp = roundf(piCpuTemp * 100) / 100; // round decimal to nearest      
+std::cout << "CPU Temperature: " << piCpuTemp << "°C\n";
