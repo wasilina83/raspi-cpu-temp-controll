d41da22877cf6abd5ebb2d4223fb8973b5a491c5
wasilina83 <103425290+wasilina83@users.noreply.github.com>
2022-10-28 02:55:01 +0200

    Update get-cpu-temp.cpp

--- get-cpu-temp.cpp
+++ get-cpu-temp.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+#include <string>
+#include <array>
+#include <vector>
 
+
+/*https://json.nlohmann.me/ leichter einlesen und sreiben */
+/*+++++++++++++++++++++++++++++++++++++++++*
+* Dieser Teil sool die CPU Temp einlesen   *
+*++++++++++++++++++++++++++++++++++++++++++*/
 
 td::string fileName = "/sys/class/thermal/thermal_zone0/temp";
 std::ifstream piCpuTempFile;
@@ -11,3 +20,13 @@ piCpuTemp = std::stof(buffer.str()); // convert string to float
 piCpuTemp = piCpuTemp / 1000; // convert float value to degree
 piCpuTemp = roundf(piCpuTemp * 100) / 100; // round decimal to nearest      
 std::cout << "CPU Temperature: " << piCpuTemp << "°C\n";
+
+/* zeit suchen und auch speichren*/
+/* temp untersuchen wenn temp <70 ok sont warnung*/
+
+
+/*+++++++++++++++++++++++++++++++++++++++++*
+* Dieser Teil sool die CPU Temp  in eine datei siechrn   *
+*++++++++++++++++++++++++++++++++++++++++++*/
+
+
